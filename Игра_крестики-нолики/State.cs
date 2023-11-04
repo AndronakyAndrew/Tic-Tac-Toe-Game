df5f4758e366ac3501354233f817ba8f4c9ed038
AndronakyAndrew <andronakya@gmail.com>
2023-11-03 23:11:48 -0400

    Add project files.

---
+++ Игра_крестики-нолики/State.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Игра_крестики_нолики
+{
+    public enum State
+    {
+        Cross,
+        Zero,
+        Unset
+    }
+}
